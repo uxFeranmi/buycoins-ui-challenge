d3adac61ae39075e191c10164aaf71aa6717fd26
Akinlade Feranmi <feranmiakinlade@gmail.com>
2019-12-09 18:54:07 +0000

    Render whole table

--- index.js
+++ index.js
@@ -5,8 +5,6 @@
  *    Get 10 coins per "page"
  */
 
-let currentPage = 0;
-
 const getData = async (start) => {
   try {
     const url = `https://api.coinlore.com/api/tickers/?start=${start}&limit=10`;
@@ -19,26 +17,39 @@ const getData = async (start) => {
   }
 };
 
-const updateTable = async () => {
+let currentPage = 0;
+
+const nextPage = async () => {
   console.log('Load!');
   const {data} = await getData(currentPage);
   if (!data)
     return window.alert('Something went wrong. Please try again.');
   
+  currentPage++;
   const tbody = document.querySelector('tbody');
   
-  let tr = document.createElement('tr');
-  let coin = document.createElement('td'); 
-  let code = document.createElement('td');
-  let price = document.createElement('td');
-  let supply = document.createElement('td');
+  for (let coin of data) {
+    let tr = document.createElement('tr');
+    let name = document.createElement('td'); 
+    let code = document.createElement('td');
+    let price = document.createElement('td');
+    let supply = document.createElement('td');
 
-  for (let coinType of data) {
-    coin.textContent = coinType.name;
+    name.textContent = coin.name;
     tr.appendChild(coin);
+
+    code.textContent = coin.symbol;
+    tr.appendChild(code);
+
+    price.textContent = '$'+coin.price_usd;
+    tr.appendChild(price);
+
+    supply.textContent = `${coin.tsupply} ${coin.symbol}`;
+    tr.appendChild(supply);
+    
     tbody.appendChild(tr);
   }
      
 };
 
-window.onload = updateTable;
+window.onload = nextPage;
